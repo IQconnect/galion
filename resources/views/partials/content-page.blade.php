48ecce8e73eddb2ad39915650f689d132ed0ee53
ralph <rafal@creativeroom.pl>
2021-01-20 11:16:11 +0100

    content

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -11,7 +11,7 @@
   <section class="section">
     <div class="container">
       <div class="text content-page">
-        <h1 class="title">
+        <h1 class="title title--center">
           {{ the_title() }}
         </h1>
         <div class="content-page__main">
